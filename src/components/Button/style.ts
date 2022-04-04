6a4d5dd25c57cdf5453073eb534de5ed764ebb95
Cassio Oliveira Silva <cos@vortx.com.br>
2022-04-04 10:16:06 -0300

    feat: add button component

---
+++ src/components/Button/style.ts
@@ -0,0 +1,22 @@
+import { shade } from 'polished';
+import styled from 'styled-components';
+import { theme } from '../../theme';
+
+const buttonColor = `${theme.colors.purple[800]}`;
+
+export const Container = styled.button`
+  margin-top: 40px;
+  background: ${buttonColor};
+  border-radius: 10px;
+  border: 0;
+  width: 100%;
+  height: 56px;
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: #fff;
+  transition: background 0.3s;
+
+  &:hover {
+    background: ${shade(0.2, buttonColor)};
+  }
+`;
